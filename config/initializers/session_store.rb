8944ab895aa0633c68b5affa4587bc8015c2598e
Nan Zhong <nan@nine27.com>
2012-07-26 00:17:24 -0400

    update session name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-GrpOfPpl::Application.config.session_store :cookie_store, key: '_together_session'
+GrpOfPpl::Application.config.session_store :cookie_store, key: '_grpofppl_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
